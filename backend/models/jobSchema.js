a24e4878a52c7bf444b0c4f25b956e79bb36f157
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-12-22 12:35:45 +0530

    Database connected and models created

---
+++ backend/models/jobSchema.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // admin
+      required: true,
+    },
+    applications: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        status: {
+          type: String,
+          enum: ["applied", "accepted", "rejected"],
+          default: "applied",
+        },
+        appliedAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Job = mongoose.model("job", jobSchema)
+export default Job;
