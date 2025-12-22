0cba7aa3a04d76b20cefb235c7567289f54e2f11
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-12-22 15:35:49 +0530

    User controller and authMiddleware created

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,95 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user";
+import { json } from "stream/consumers";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+
+    if (!email || !name || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User Already Exists!!" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+    res.status(201),json({
+      message: "User Created",
+      token,
+      newUser: {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+      }
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error - Cannot register" });
+    console.log("Error in register controller", error);
+  }
+};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.SECRET_KEY, {
+      expiresIn: "7d",
+    });
+    res.status(201),json({
+      message: "Login Successfully",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      }
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Internal Server Error - Cannot login" });
+    console.log("Error in login controller", error);
+  }
+};
