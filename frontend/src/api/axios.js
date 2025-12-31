609f3366845ec918f8b7bd1c5ececc26424e08bc
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-12-31 17:08:13 +0530

    Update BASE_URL to use production API endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://jobboard-27cx.onrender.com/api"
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
