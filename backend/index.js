2521d6e13a514ec6bdf0e594910165ba94faf72e
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-12-31 16:39:42 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import connectDb from "./config/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/db.js";
 
-import jobRouter from "./routes/job.route.js"
-import authRouter from "./routes/user.route.js"
+import jobRouter from "./routes/job.route.js";
+import authRouter from "./routes/user.route.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-app.use("/api/job", jobRouter)
-app.use("/api/auth", authRouter)
+app.use("/api/job", jobRouter);
+app.use("/api/auth", authRouter);
 
-app.listen(PORT, ()=>{
-  console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
   connectDb();
-})
+});
